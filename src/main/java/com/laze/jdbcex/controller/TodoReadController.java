23f9c34cce12eda16c74009348dcd35e9139e83e
L-a-z-e <yysi8771@gmail.com>
2024-09-08 04:41:18 +0900

    controller 개선 전

--- src/main/java/com/laze/jdbcex/controller/TodoReadController.java
+++ src/main/java/com/laze/jdbcex/controller/TodoReadController.java
@@ -3,32 +3,34 @@ package com.laze.jdbcex.controller;
 import com.laze.jdbcex.dto.TodoDTO;
 import com.laze.jdbcex.service.TodoService;
 import lombok.extern.log4j.Log4j2;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "TodoListController", value = "/todo/list")
+@WebServlet(name="todoReadController", value = "/todo/read")
 @Log4j2
-public class TodoListController extends HttpServlet {
+public class TodoReadController extends HttpServlet {
 
-    private final TodoService todoService = TodoService.INSTANCE;
+    private TodoService todoService = TodoService.INSTANCE;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        log.info("todo list...");
-
         try {
-            List<TodoDTO> dtoList = todoService.listAll();
-            req.setAttribute("dtoList", dtoList);
-            req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+            Long tno = Long.parseLong(req.getParameter("tno"));
+
+            TodoDTO todoDTO = todoService.get(tno);
+
+            req.setAttribute("dto", todoDTO);
+            req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
         } catch (Exception e) {
             log.error(e.getMessage());
-            throw new ServletException("list error");
+            throw new ServletException("read error");
         }
     }
+
 }
--- src/main/java/com/laze/jdbcex/service/TodoService.java
+++ src/main/java/com/laze/jdbcex/service/TodoService.java
@@ -6,7 +6,6 @@ import com.laze.jdbcex.dto.TodoDTO;
 import com.laze.jdbcex.util.MapperUtil;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,7 +28,6 @@ public enum TodoService {
 
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
 
-        //System.out.println("todoVO : " + todoVO);
         log.info(todoVO);
 
         todoDAO.insert(todoVO);
@@ -47,5 +45,28 @@ public enum TodoService {
                 .collect(Collectors.toList());
 
         return dtoList;
+    }
+
+    public TodoDTO get(Long tno) throws Exception {
+
+        log.info("tno : " + tno);
+        TodoVO todoVO = todoDAO.selectOne(tno);
+        TodoDTO todoDTO = modelMapper.map(todoVO, TodoDTO.class);
+
+        return todoDTO;
+    }
+
+    public void remove(Long tno) throws Exception {
+
+        log.info("tno : " + tno);
+        todoDAO.deleteOne(tno);
+    }
+
+    public void modify(TodoDTO todoDTO) throws Exception {
+
+        log.info("todoDTO : " + todoDTO);
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+
+        todoDAO.updateOne(todoVO);
     }
 }
